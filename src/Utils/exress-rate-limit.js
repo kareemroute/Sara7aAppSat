cbc2aadab774c0458039edb85c898c3001be3ffe
Karim033 <gaberk033@gmail.com>
2025-09-02 22:26:24 +0300

    Edit on rate limit

--- src/Utils/exress-rate-limit.js
+++ src/Utils/exress-rate-limit.js
@@ -2,8 +2,8 @@ import { rateLimit } from "express-rate-limit";
 
 export function limiter() {
   const limiter = rateLimit({
-    windowMs: 60 * 1000,
-    limit: 3,
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
     message: {
       statusCode: 429,
       message: "Too many requests from this IP, please try again",
